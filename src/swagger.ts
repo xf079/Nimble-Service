241642f89613e36f33f2bc502b97de2d94c45902
XueFeng Li <lance.li@yimlink.com>
2024-12-26 17:51:14 +0800

    feat: init

---
+++ src/swagger.ts
@@ -0,0 +1,13 @@
+import type { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app) {
+  const options = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
